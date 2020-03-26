24be4443cc227b4c655a62045c07958a03de106b
alliendeveloper <52082507+Bello2609@users.noreply.github.com>
2020-03-26 17:29:34 +0100

    Add files via upload

---
+++ js task.js
@@ -0,0 +1,13 @@
+//my first javascript task
+function jstask1(){
+let name = "Bello Hadi";
+let myCourses = ["html", "css", "javascript"];
+    document.write("my name and courses :" + name + myCourses);
+//since myCourses is odd then i have to list  all odd numbers from 1- 200
+for(let i = 0; i < 200 ; i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+} 
+}
+ 
